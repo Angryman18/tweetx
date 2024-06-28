b553c2699bd65a361626ce3177e8c52a91d05e46
Shyam Mahanta <smahanta118@gmail.com>
2024-06-28 12:48:30 +0530

    changed server to express

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const mongoose = require("mongoose");
 const dotEnv = require("dotenv");
 const next = require("next");
-const { createServer } = require("http");
+const express = require("express");
 
 dotEnv.config({ path: "./.env.local" });
 const dev = process.env.NODE_ENV === "development";
 const app = next({ dev, port: 3001 });
 const handler = app.getRequestHandler();
+const expressApp = express();
 
-console.log("Environment ", process.env.NODE_ENV)
+console.log("Environment ", process.env.NODE_ENV);
 
 app.prepare().then(() => {
-  const server = createServer(async (req, res) => {
+  expressApp.use(async (req, res) => {
     try {
       await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI);
-      await handler(req, res);
+      handler(req, res);
     } catch (err) {
       process.exit(1);
     }
   });
-  server.listen(3001, () => {
+  expressApp.listen(3001, () => {
     console.log("Server is runnning at 3001");
   });
 });
