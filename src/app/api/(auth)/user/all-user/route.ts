70d35407224869ddd2751aeeefce16108cabe711
Shyam Mahanta <smahanta118@gmail.com>
2024-06-29 14:36:13 +0530

    fix mongoose error

--- src/app/api/(auth)/user/all-user/route.ts
+++ src/app/api/(auth)/user/all-user/route.ts
@@ -3,10 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   try {
-    const getAllUsers = await User.find({});
+    // const getAllUsers = await User.find({});
     //   .select("fullname followersCount followingCount postCount")
-    console.log(getAllUsers);
-    return NextResponse.json(getAllUsers);
+    // console.log(getAllUsers);
+    return NextResponse.json({message: "Hello world"});
   } catch (err) {
     console.log(err);
     return NextResponse.json({ error: "Error Occured" }, { status: 401 });
