a706edafb50cc0150654fad04faf4e710e4d0ac1
Shyam Mahanta <smahanta118@gmail.com>
2024-07-20 10:37:17 +0530

    date fix

--- src/utils/fe-utils/commonUtil.ts
+++ src/utils/fe-utils/commonUtil.ts
@@ -2,11 +2,12 @@ export const calculatePostTime = (time: string) => {
   /* @ts-ignore */
   const timeage = new Date(new Date().toISOString()) - new Date(time);
   const minutes = timeage / (1000 * 60);
+  console.log(minutes);
   const hours =
-    minutes > 60
-      ? `${Math.floor(minutes / 60)} ${minutes < 120 ? "hour" : "hours"} ago`
-      : minutes > 24 * 60 && minutes < 48 * 60
+    minutes > 24 * 60 && minutes < 48 * 60
       ? "1 day ago"
+      : minutes > 60 && minutes < 24 * 60
+      ? `${Math.floor(minutes / 60)} ${minutes < 120 ? "hour" : "hours"} ago`
       : minutes > 24 * 60
       ? new Date(time).toDateString()
       : `${Math.floor(minutes)} minutes ago`;
